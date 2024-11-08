e45c643e5e1ae363d4db12f477f4189f85b10a5c
unknown <sharksoareskosvoski@gmail.com>
2024-11-08 00:02:57 -0400

    Finalizando formulário

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ function RolarParaDireita() {
     Leonardo.style = "display:none"
     Bruna.style = "display:flex"
     SetaDireita.style = "display:none"
-    SetaEsquerda.style = "display:flex"
+    SetaEsquerda.style = "display:flex; margin-top:55px"
 }
+
+function RolarParaEsquerda() {
+    Leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    SetaDireita.style = "display:flex; margin-top:55px"
+    SetaEsquerda.style = "display:none"
+
+}   
